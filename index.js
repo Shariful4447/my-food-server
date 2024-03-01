be5aff9a6396fdb09e0779f64f56b55f171ea310
Shariful4447 <shariful.islam4447@gmail.com>
2024-03-01 03:47:03 +0100

    user role set as admin + delete user function done

--- index.js
+++ index.js
@@ -34,6 +34,27 @@ async function run() {
     const cartCollection  = client.db("foodDb").collection("cart");
 
     //user req
+    app.get('/users', async (req, res) =>{
+        const result = await userCollection.find().toArray();
+        res.send(result);
+    })
+    app.delete('/users/:id', async (req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+    app.patch('/users/admin/:id', async (req, res) =>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc = {
+        $set:{
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
     app.post('/users', async (req, res) =>{
         const user = req.body;
         const result = await userCollection.insertOne(user);
